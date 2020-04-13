f915fd447936f58ede2b6d7a004c78a1a4fe0528
miguelgomez <miguel_gomezjr@hotmail.com>
2020-04-13 18:26:20 +0200

    tp2_1_1

---
+++ tp2_1_1.cpp
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define N 4
+#define M 5
+
+
+int main(){
+
+int f,c;
+double mt[N][M];
+
+for(f = 0;f<N; f++){
+    for(f = 0;f<N; f++){
+        printf("%lf   ", mt[f][c]);
+    }
+    printf("\n");
+}
+
+
+
+
+
+
+
+    return 0;
+}
